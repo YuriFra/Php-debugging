7490642807d431b2d15ff092e10c53b84969d6ad
becode <yf_be@yahoo.co.uk>
2020-07-17 13:30:57 +0200

    debugged

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Php-debugging
 Debugging exercises in Php
+
+- Repository: `php-debugging`
+- Type of Challenge: `Learning challenge`
+- Duration: `1 day`
+- Team challenge : `solo`
+
+### Learning objectives
+- understanding the root of bug fixing
+- know what print_r, var_dump, die, echo, exit, break do
+- know what to do the next time you're stuck
+- give yourself the `solver's mentality`, a problem is just an opportunity to learn!
+
+### The Mission
+Debugging is the pinpoint to being a master in any coding language.  
+Everyone gets errors, but the best programmers never let it stop them and they find a way to fix it every time!
+Debugging guide: [The guide](https://rollbar.com/guides/how-to-debug-php/)
+
+Discover, look up, research whatever ways work for you to debug you code!
+
+**Use xDebug as much as possible!**
---
+++ index.php
@@ -0,0 +1,166 @@
+<?php
+declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Below are several code blocks, read them, understand them and try to find whats wrong.
+// Try to fix the code every time and good luck! (write down how you found out the answer and how you debugged the problem)
+//=========================================================================================================
+
+// === Exercise 1 ===
+// Below we're defining a function, but it doesn't work when we run it.
+// Look at the error you get, read it and it should tell you the issue...,
+// sometimes, even your IDE can tell you what's wrong
+echo "Exercise 1 starts here:";
+
+function new_exercise($x) {
+    $block = "<br><br/><hr/><br/>Exercise $x starts here:<br/><br>";
+    echo $block;
+}
+new_exercise(1);
+//solution: add a variable as a parameter
+
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0];
+echo ucfirst($monday);
+//solution: monday is the first element of the array with index 0
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = 'Debugged ! Also very fun';
+echo substr($str, 0, 10);
+//solution: single quotes around string
+
+
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
+//solution: add & before the value to reference to the new value in the array
+
+
+new_exercise(5);
+// === Exercise 5 ===
+// The array should be printing every letter of the alfabet (a-z) but instead it does that + aa-yz
+// Fix the code so the for loop only pushes a-z in the array
+
+$arr = [];
+for ($letter = 'a'; $letter <= 'z'; $letter++) {
+    $letter = range('a', 'z');
+    array_push($arr, $letter);
+}
+print_r($arr); // Array ([0] => a, [1] => b, [2] => c, ...) a-z alfabetical array
+//solution: add range to elements of array
+
+
+new_exercise(6);
+// === Final exercise ===
+// The fixed code should echo the following at the bottom:
+// Here is the name: $name - $name2
+// $name variables are decided as seen in the code, fix all the bugs whilst keeping the functionality!
+$arr = [];
+function combineNames($str1 = "", $str2 = "") {
+    $params = [$str1, $str2];
+    foreach($params as &$param) {
+        if ($param == "") {
+            $param = randomHeroName();
+        }
+    }
+    return implode(" - ", $params);
+}
+function randomGenerate($arr, $amount) {
+    for ($i = $amount; $i > 0; $i--) {
+        array_push($arr, randomHeroName());
+    }
+    return $amount;
+}
+function randomHeroName() {
+    $hero_firstnames = ["captain", "doctor", "iron", "Hank", "ant", "Wasp", "the", "Hawk", "Spider", "Black", "Carol"];
+    $hero_lastnames = ["America", "Strange", "man", "Pym", "girl", "hulk", "eye", "widow", "panther", "daredevil", "marvel"];
+    $heroes = [$hero_firstnames, $hero_lastnames];
+    $randname = $heroes[rand(0,count($heroes)-1)][rand(0, 10)];
+    return $randname;
+}
+echo "Here is the name: " . combineNames();
+//solution: return $randname & implode instead of echo, in foreach $param needs to be overwritten, count $heroes is 2
+
+
+new_exercise(7);
+function copyright(int $year) {
+    echo "&copy; $year BeCode";
+}
+//print the copyright
+copyright((int)date('Y'));
+//solution: add (int) to Y for year and echo result in function instead of return
+
+
+new_exercise(8);
+function login(string $email, string $password) {
+    if($email == 'john@example.be' && $password == 'pocahontas') {
+        return 'Welcome John Smith<br>';
+    }
+    return 'No access<br>';
+}
+
+//do not change anything below
+//should great the user with his full name (John Smith)
+echo login('john@example.be', 'pocahontas');
+//no access
+echo login('john@example.be', 'dfgidfgdfg');
+//no access
+echo login('wrong@example.be', 'wrong');
+//solution: 2 returns overwrite each other so put in 1 return, && instead of || email & password should be correct
+
+
+new_exercise(9);
+function isLinkValid(string $link) {
+    $unacceptables = array('https:','.doc','.pdf', '.jpg', '.jpeg', '.gif', '.bmp', '.png');
+    foreach ($unacceptables as $unacceptable) {
+        if (strpos($link, $unacceptable) !== false) {
+            return 'Unacceptable Found<br />';
+        }
+    }
+    return 'Acceptable<br />';
+}
+//invalid link
+echo isLinkValid('http://www.google.com/hack.pdf');
+//invalid link
+echo isLinkValid('https://google.com');
+//VALID link
+echo isLinkValid('http://google.com');
+//VALID link
+echo isLinkValid('http://google.com/test.txt');
+//solution: echo results + strpos !== false instead of true
+
+
+new_exercise(10);
+//Filter the array $areTheseFruits to only contain valid fruits
+//do not change the arrays itself
+$areTheseFruits = ['apple', 'bear', 'beef', 'banana', 'cherry', 'tomato', 'car'];
+$validFruits = ['apple', 'pear', 'banana', 'cherry', 'tomato'];
+//from here on you can change the code
+$length = count($areTheseFruits);
+for($i = 0;  $i < $length; $i++) {
+    if(!in_array($areTheseFruits[$i], $validFruits, $strict = true)) {
+        unset($areTheseFruits[$i]);
+    }
+}
+var_dump($areTheseFruits);//do not change this
+//solution: $i < ipv <=, $strict = true, counter needs to decrease when values of array decrease
